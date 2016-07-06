1a3ffcd052acc1f01dc7348f239b1e5f3362b422
Mark Litchfield <mlitchfield@timerazor.com>
2016-07-06 12:03:46 -0400

    Corrects binding for chartData to support $onChanges event. Cleans up source layout to conform to JSCS configuration. Corrects missing dependency injections.

--- ng-epoch.js
+++ ng-epoch.js
@@ -13,7 +13,7 @@
     chartInner: '=',
     chartWidth: '=',
     chartHeight: '=',
-    chartData: '=',
+    chartData: '<',
     chartClass: '=',
     chartOrientation: '=',
     chartPadding: '=',
@@ -37,35 +37,30 @@
     gaugeStream: '<?'
   };
 
-  function EpochController($scope, $element) {
-    var ec = angular.extend(this, {
-      options: this.options || {},
-      renderEpoch: function() {
-        var container = angular.element($element[0].childNodes[0]);
-        var type = $element[0].nodeName.toLowerCase().replace('epoch-', '').replace('live-', 'time.');
-        this.options.type = (type === 'gauge' || type === 'heatmap') ? 'time.' + type : type;
-        if (this.chartClass) {
-          container.addClass(this.chartClass);
-        }
-        if (this.options.type === 'time.gauge') {
-          container.addClass(this.gaugeDialSize || 'gauge-small');
-        }
-        this.epochObj = container.epoch(this.options);
-      },
-      filterOptions: function() {
-        var _self = this;
-        if (!_self.options.length) {
-          angular.forEach(this, function(v, k) {
-            var validKey = ((k.indexOf('chart') > -1 || k.indexOf('gauge') > -1) && (k !== 'chartClass' && k !== 'gaugeDialSize'));
-            if (angular.isDefined(v) && validKey) {
-              var newkey = k.substring(5);
-              this[newkey.charAt(0).toLowerCase() + newkey.slice(1)] = v;
-            }
-          }, _self.options);
-        }
+  var EpochController = function($scope, $element) {
+    this.options = this.options || {};
+    this.renderEpoch = function() {
+      var container = angular.element($element[0].childNodes[0]);
+      var type = $element[0].nodeName.toLowerCase().replace('epoch-', '').replace('live-', 'time.');
+      this.options.type = (type === 'gauge' || type === 'heatmap') ? 'time.' + type : type;
+      if (this.chartClass) {
+        container.addClass(this.chartClass);
       }
-    });
-    ec.$onChanges = function() {
+      if (this.options.type === 'time.gauge') {
+        container.addClass(this.gaugeDialSize || 'gauge-small');
+      }
+      this.epochObj = container.epoch(this.options);
+    };
+    this.filterOptions = function() {
+      angular.forEach(this, function(v, k) {
+        var validKey = ((k.indexOf('chart') > -1 || k.indexOf('gauge') > -1) && (k !== 'chartClass' && k !== 'gaugeDialSize'));
+        if (angular.isDefined(v) && validKey) {
+          var newkey = k.substring(5);
+          this[newkey.charAt(0).toLowerCase() + newkey.slice(1)] = v;
+        }
+      }, this.options);
+    };
+    this.$onChanges = function() {
       if (this.epochObj) {
         if (this.chartStream || this.gaugeStream) {
           if (this.chartStream) { this.epochObj.push(this.chartStream); }
@@ -75,17 +70,19 @@
         }
       }
     };
-    ec.$postLink = function() {
-      ec.filterOptions();
-      ec.renderEpoch();
+    this.$postLink = function() {
+      this.filterOptions();
+      this.renderEpoch();
     };
-  }
+  };
 
   var baseComponent = {
     controller: EpochController,
     bindings: allOptions,
     template: '<div class="epoch"></div>'
   };
+
+  EpochController.$inject = ['$scope', '$element'];
 
   angular.module('ng.epoch', [])
     .component('epochArea', baseComponent)
